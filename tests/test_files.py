1f38a5beab5cc15c8fdd9796da053fee0dfb092a
dhruvix <dhruvix7@gmail.com>
2026-09-13 21:13:55 -0700

    fix test name mismatch

--- tests/test_files.py
+++ tests/test_files.py
@@ -25,7 +25,7 @@ def test_write_and_read_config(tmp_path: Path):
     assert Path(files.read_config(config, "JOT_PATH")) == jot
 
 
-def test_write_jotting_prepends(tmp_path: Path):
+def test_multiline_jotting(tmp_path: Path):
     def fixed_now():
         return dt.datetime(2025, 12, 25, 1, 0)
 
@@ -37,7 +37,7 @@ def test_write_jotting_prepends(tmp_path: Path):
     content = jot.read_text()
     assert content == "[2025-12-25 01:00] foo bar\n"
 
-def test_multiline_jotting(tmp_path: Path):
+def test_write_jotting_prepends(tmp_path: Path):
     def fixed_now():
         return dt.datetime(2025, 12, 25, 1, 0)
 
